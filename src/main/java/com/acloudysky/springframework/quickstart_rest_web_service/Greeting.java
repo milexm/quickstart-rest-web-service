b9269c9b885e653f64d02995f2e16093a0d3ad34
Michael <milexm@gmail.com>
2018-09-09 20:48:53 -0700

    First commit

---
+++ src/main/java/com/acloudysky/springframework/quickstart_rest_web_service/Greeting.java
@@ -0,0 +1,20 @@
+package com.acloudysky.springframework.quickstart_rest_web_service;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
